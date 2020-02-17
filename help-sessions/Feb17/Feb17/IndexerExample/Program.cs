9e44e326f1920629cff8639714b9d9cd0094400b
Keith Leonard Mannock <keith@dcs.bbk.ac.uk>
2020-02-17 15:21:25 +0000

    Sync from Feb17 afternoon help session

---
+++ help-sessions/Feb17/Feb17/IndexerExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IndexerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var s = new StringData();
+           s[0] = "Some string";
+           s[1] = "Another string";
+           s[9] = "foo";
+
+           for (var i=0; i < s.Size; i++)
+           {
+               Console.WriteLine(s[i]);
+           }
+        }
+    }
+
+    class StringData
+    {
+        private string[] strArr = new string[10];
+        
+        public int Size => strArr.Length;
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= strArr.Length)
+                {
+                    throw new IndexOutOfRangeException("some message - get");
+                }
+
+                return strArr[index];
+            }
+            set
+            {
+                if (index < 0 || index >= strArr.Length)
+                {
+                    throw new IndexOutOfRangeException("some message - set");
+                }
+
+                strArr[Size-index-1] = value;
+            }
+        }
+    }
+}
---
+++ help-sessions/Feb17/Feb17/PredsVsDelegatesAndThis/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace PredsVsDelegates
+{
+    public static class StringExtensions
+    {
+        public static bool IsLowerCase(this string str) => str.Equals(str.ToLower());
+    }
+}
