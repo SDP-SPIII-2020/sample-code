8ce50cd2ed13d601465d552bb8ae62be387acde5
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-02-17 21:11:23 +0000

    Feb 17 evening

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
