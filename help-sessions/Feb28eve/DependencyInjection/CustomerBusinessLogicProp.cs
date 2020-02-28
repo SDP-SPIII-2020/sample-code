8d568a1c2bb7902a31db263efadca0212294b8b1
Keith Leonard Mannock <keith@dcs.bbk.ac.uk>
2020-02-28 19:40:51 +0000

    reorganised help sessions folder

---
+++ help-sessions/Feb28eve/DependencyInjection/CustomerBusinessLogicProp.cs
@@ -0,0 +1,8 @@
+﻿namespace DependencyInjection
+{
+    public class CustomerBusinessLogicProp
+    {
+        public ICustomerDataAccess DataAccess { get; set; }
+        public string GetCustomerName(int id) => DataAccess.GetCustomerName(id);
+    }
+}
