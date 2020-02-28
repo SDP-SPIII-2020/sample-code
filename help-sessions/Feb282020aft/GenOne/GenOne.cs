5c7de62fb91ff15000db00c4fc535e3d4a3ac46a
Keith Leonard Mannock <keith@dcs.bbk.ac.uk>
2020-02-28 19:37:31 +0000

    Code examples from the help sessions on the 28th Feb

---
+++ help-sessions/Feb282020aft/GenOne/GenOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenOne
+{
+    class GenOne
+    {
+        static void Main(string[] args)
+        {
+            var items = GenerateNames();
+            PrintNames(items);
+        }
+
+        static string[] GenerateNames() // function
+        { // need to know the size of the array at creation time
+            string[] _names = new string[3];
+            _names[0] = "Fred";
+            _names[1] = "Betty";
+            _names[2] = "Colin";
+            return _names;
+        }
+
+        static void PrintNames(string[] names) // procedure
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Item is {name}");
+            }
+        }
+    }
+}
---
+++ help-sessions/Feb282020aft/GenSix/Constraints.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenSix
+{
+    class Constraints
+    {
+        static void Main(string[] args)
+        {
+            var aList = new List<IThing>();
+            aList.Add(new A());
+            Method(aList);
+            
+            var bList = new List<B>();
+            bList.Add(new B(34));
+            Method(bList);
+            
+            var cList = new List<C>();
+            cList.Add(new C());
+            //Method(cList);
+
+            var sillyList = new List<bool>();
+            Init(sillyList);
+            Method(sillyList);
+        }
+
+        static void Method<T>(List<T> aList)
+        {
+            aList.ForEach(x => Console.WriteLine($"{x}"));
+            Console.WriteLine($"typeof(T) = ${typeof(T)}");
+        }
+
+        static void Init<T>(List<T> aList) where T : notnull
+        {
+            aList.Add(default(T)); // = new T();
+        }
+        
+        // where T : class
+        // where T : struct
+        
+        // Outer<string>.Inner<string>
+        // Outer<string>.Inner<int>
+    }
+
+    interface IThing 
+    {}
+    
+    class A: IThing
+    {}
+
+    class B : IThing
+    {
+        internal B(int x)
+        {
+        }
+    }
+    
+    class C
+    {}
+
+    class Outer<TOuter>
+    {
+        internal class Inner<TInner>
+        {
+            static int value;
+        }
+    }
+    class LinkedList<T>
+    {
+        class Node<T>
+        {
+        
+        }
+    }
+}
+
