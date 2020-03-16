8b34d1b5a56254349bc011eed76c0f064d1342a4
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-03-16 15:00:36 +0000

    Dive into Design Patterns Book Code

--- help-sessions/Mar16/Mar16/Fraction.cs
+++ help-sessions/Mar16/Mar16/Fraction.cs
@@ -1,7 +1,42 @@
+using System;
+
 namespace Mar16
 {
-    public class Fraction
+    public interface IFraction
     {
-        
+        public IFraction Add(IFraction f);
+        public static IFraction operator +(IFraction f1, IFraction f2) => f1.Add(f2);
+    }
+
+    public readonly struct Fraction : IFraction
+    {
+        private int One { get; }
+        private int Two { get; }
+
+        public Fraction(int one = 0, int two = 1)
+        {
+            One = one;
+            Two = two;
+        }
+
+        public IFraction Add(IFraction f)
+        {
+            var fraction = f as Fraction? ?? default;
+            return new Fraction(this.One + fraction.One, this.Two + fraction.Two);
+        }
+
+        public override string ToString() => $"One: {One} and Two: {Two}";
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            IFraction f1 = new Fraction(1,2);
+            IFraction f2 = new Fraction(1,2);
+            //var f3 = f1.Add(f2);
+            var f3 = f1 + f2;
+            Console.WriteLine(f3);
+        }
     }
 }
