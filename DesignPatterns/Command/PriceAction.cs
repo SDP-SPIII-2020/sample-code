2088e2aef833e66dbe87f6cef38861d2c484aa54
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-03-22 09:26:24 +0000

    Command design pattern example

---
+++ DesignPatterns/Command/PriceAction.cs
@@ -0,0 +1,24 @@
+namespace Command
+{
+    public enum PriceAction
+    {
+        Increase,
+        Decrease
+    }
+
+    /*
+    // only use this code pattern if there are methods/properties involved
+    public interface IPriceAction
+    {
+    }
+
+    sealed class Increase : IPriceAction
+    {
+    }
+
+    sealed class Decrease : IPriceAction
+    {
+        
+    }
+    */
+}
