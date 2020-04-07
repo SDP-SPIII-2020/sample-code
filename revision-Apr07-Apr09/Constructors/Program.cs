5157dfe6a1c0e0a184ebbd2c3d7026b2117afc4f
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-04-07 16:36:03 +0100

    sync

---
+++ revision-Apr07-Apr09/Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = default(string);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Parent
+    {
+        public Parent(string s)
+        {
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child() : base(default(string)) // okay until base constructor is private
+        {
+        }
+    }
+}
